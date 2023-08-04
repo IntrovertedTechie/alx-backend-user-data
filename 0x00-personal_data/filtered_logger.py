18eac2dfd66a43411a970b23c6871642e255ea66
Adio Adebayo <zillionaire234@gmail.com>
2023-08-04 01:28:44 +0100

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -22,6 +22,7 @@ def filter_datum(fields: List[str], redaction: str,
 # Function to configure and return the logger
 
 
+
 def get_logger() -> logging.Logger:
     """Returns a configured logger object."""
     logger = logging.getLogger('user_data')
