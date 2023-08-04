10922f4fe782913390d64e9519eb1dea55e4d887
Introvertedtechie <zillionaire234@gmail.com>
2023-08-04 00:43:18 +0000

    data

--- 0x00-personal_data/bo.py
+++ 0x00-personal_data/bo.py
@@ -1,29 +1,24 @@
 #!/usr/bin/env python3
-""" Protecting Personally Identifiable Information (PII) """
 
+"""PII Obfuscation"""
+
+from typing import List
 import logging
 import re
-from typing import List
 from mysql.connector import connection
 from os import environ
 
-
-# List of PII fields to obfuscate
 PII_FIELDS = ('name', 'email', 'password', 'ssn', 'phone')
 
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """Obfuscate sensitive data"""
+    temp = message
+    for field in fields:
+        temp = re.sub(field + "=.*?" + separator, field + "=" + redaction + separator, temp)
+    return temp
 
-# Function to obfuscate sensitive PII data in the log message
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """Returns the log message with sensitive fields obfuscated."""
-    pattern = r'(?<={}=).*?(?={})'.format(separator.join(fields),
-                                          re.escape(separator))
-    return re.sub(pattern, redaction, message)
-
-
-# Function to configure and return the logger
 def get_logger() -> logging.Logger:
-    """Returns a configured logger object."""
+    """Create & configure logger"""
     logger = logging.getLogger('user_data')
     logger.setLevel(logging.INFO)
     logger.propagate = False
@@ -33,10 +28,8 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
     return logger
 
-
-# Function to establish a connection to the MySQL database
 def get_db() -> connection.MySQLConnection:
-    """Connects to the MySQL server using environmental variables."""
+    """Connect to MySQL"""
     username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
     password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
     db_host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
@@ -48,34 +41,26 @@ def get_db() -> connection.MySQLConnection:
         database=db_name)
     return connector
 
-
-# Custom Formatter class to redact sensitive PII data in logs
 class RedactingFormatter(logging.Formatter):
-    """Custom formatter to redact sensitive fields in log records."""
-
+    """Custom log formatter"""
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """Initializes the RedactingFormatter class instance."""
+        """Init RedactingFormatter"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """Filters values in incoming log records."""
+        """Filter incoming logs"""
         return filter_datum(
             self.fields, self.REDACTION, super(
                 RedactingFormatter, self).format(record),
             self.SEPARATOR)
 
-
-# Main function to retrieve user data from theI
 def main() -> None:
-    """
-    Connect to the database using get_db
-.
-    """
+    """Retrieve & log user data"""
     db = get_db()
     cur = db.cursor()
 
@@ -87,14 +72,12 @@ def main() -> None:
 
     for row in fetch_data:
         fields = 'name={}; email={}; phone={}; ssn={}; password={}; ip={}; ' \
-                 'last_login={}; user_agent={};'
-        fields = fields.format(row[0], row[1], row[2], row[3],
-                               row[4], row[5], row[6], row[7])
+            'last_login={}; user_agent={};'
+        fields = fields.format(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
         logger.info(fields)
 
     cur.close()
     db.close()
-
 
 if __name__ == "__main__":
     main()
