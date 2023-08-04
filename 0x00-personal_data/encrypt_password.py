0e94b47bdffa1e2a26bd2cbb33dd3c9736ab55a1
Introvertedtechie <zillionaire234@gmail.com>
2023-08-04 01:04:51 +0000

    IBB

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
     """
     Hash the password using bcrypt and return the hashed password as bytes.
@@ -8,6 +9,7 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """
