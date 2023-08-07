6afe62e26ed19fd13c2bf875819a5d5f623ec510
Adio Adebayo <zillionaire234@gmail.com>
2023-08-07 22:59:40 +0100

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,6 +22,14 @@ def unauthorized(error) -> str:
     return jsonify({"error": "Unauthorized"}), 401
 
 
+# Add the new error handler for 403 
+ @app.errorhandler(403) 
+ def Forbidden(error) -> str: 
+     """ Forbidden 
+     """ 
+     return jsonify({"error": "Forbidden"}), 403 
+ 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found 
