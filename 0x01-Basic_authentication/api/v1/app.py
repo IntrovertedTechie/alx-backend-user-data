a7dbcd7d0ea531288c96a95c0de01ef516193cc1
Adio Adebayo <zillionaire234@gmail.com>
2023-08-07 21:15:20 +0100

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,17 +14,20 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
-
-
 # Add the new error handler for 401 Unauthorized
 @app.errorhandler(401)
 def unauthorized_error(error):
     return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found 
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+
 
 
 if __name__ == "__main__":
