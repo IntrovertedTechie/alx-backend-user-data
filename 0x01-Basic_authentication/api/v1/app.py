d5f84a58b8689f3c7bc871eceb2ac5589f1710f8
Adio Adebayo <zillionaire234@gmail.com>
2023-08-08 09:41:59 +0100

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,6 +14,10 @@ app = Flask(__name__)
 app.register_blueprint(app_views) 
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}}) 
 
+
+
+
+
 @app.errorhandler(401) 
 def unauthorized(error) -> str: 
     """ Unauthorized 
