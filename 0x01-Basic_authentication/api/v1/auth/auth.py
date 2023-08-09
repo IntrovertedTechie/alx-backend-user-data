129c6eff2297295a99c095da4402614d3c6f58d0
Introvertedtechie <zillionaire234@gmail.com>
2023-08-09 09:43:27 +0000

    grace

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,45 +6,41 @@ API authentication module
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """
-    Base class for authentication
+     class
     """
+    
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Checks if API routes require authentication
-
+        
         Args:
             path (str): The API route path
             excluded_paths (List[str]): List of excluded paths
-
+        
         Returns:
             bool: True if authentication is required, False otherwise
         """
         if path is None:
             return True
-
-        if excluded_paths is None or not isinstance(excluded_paths, list):
+        if excluded_paths is None or not excluded_paths:
             return True
-
-        for excluded_path in excluded_paths:
-            if excluded_path.endswith("*"):
-                prefix = excluded_path[:-1]  # Remove the *
-                if path.startswith(prefix):
-                    return False
-            elif path == excluded_path:
+        for i in excluded_paths:
+            if i.endswith('/') and (path == i or path.startswith(i[:-1])):
                 return False
-
         return True
+
 
     def authorization_header(self, request=None) -> str:
         """
         Checks if Authorization request header is present & contains values
-
+        
         Args:
             request: Flask request object
-
+        
         Returns:
             str: Authorization header value if present, otherwise None
         """
@@ -53,13 +49,14 @@ class Auth:
         else:
             return request.headers.get('Authorization')
 
+
     def current_user(self, request=None) -> TypeVar('User'):
         """
         Placeholder for current user information
-
+        
         Args:
             request: Flask request object
-
+        
         Returns:
             TypeVar('User'): Current user information or None
         """
