17a7d4b4f40f516971aea354cd5539bbb8d2ffa0
Adio Adebayo <zillionaire234@gmail.com>
2023-08-10 14:19:03 +0100

    Update users.py

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -5,6 +5,7 @@ from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
 
+
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def view_all_users() -> str:
     """ GET /api/v1/users
@@ -13,6 +14,7 @@ def view_all_users() -> str:
     """
     all_users = [user.to_json() for user in User.all()]
     return jsonify(all_users)
+
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
@@ -35,6 +37,7 @@ def view_one_user(user_id: str = None) -> str:
     return jsonify(user.to_json())
 
 # ... (other routes)
+
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def update_user(user_id: str = None) -> str:
