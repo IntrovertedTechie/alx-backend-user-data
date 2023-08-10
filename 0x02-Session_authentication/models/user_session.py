e293adbda206495fc0450491d05c54836c025d42
Adio Adebayo <zillionaire234@gmail.com>
2023-08-10 23:39:21 +0100

    Update user_session.py

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
+""" UserSession module
+"""
 from models.base import Base
+
 
 class UserSession(Base):
     """
-    Model class for UserSession that inherits from Base
+    UserSession class
     """
+
     def __init__(self, *args: list, **kwargs: dict):
         """
-        Initialize the UserSession instance
-        Args:
-            args: List of arguments
-            kwargs: Dictionary of keyword arguments
+        Initialize a UserSession instance
         """
         super().__init__(*args, **kwargs)
-        self.user_id = kwargs.get("user_id")
-        self.session_id = kwargs.get("session_id")
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
