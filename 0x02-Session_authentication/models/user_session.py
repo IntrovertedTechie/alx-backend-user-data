abfe0c46f793be80eeee5a03eeccc63fdccf6b8d
Introvertedtechie <zillionaire234@gmail.com>
2023-08-10 22:26:32 +0000

    good night

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from models.base import Base
+
+class UserSession(Base):
+    """
+    Model class for UserSession that inherits from Base
+    """
+    def __init__(self, *args: list, **kwargs: dict):
+        """
+        Initialize the UserSession instance
+        Args:
+            args: List of arguments
+            kwargs: Dictionary of keyword arguments
+        """
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get("user_id")
+        self.session_id = kwargs.get("session_id")
