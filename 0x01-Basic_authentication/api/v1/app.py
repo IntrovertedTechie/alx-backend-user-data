c1d0696aae2caf1a131434df399fdb14bec670cd
Adio Adebayo <zillionaire234@gmail.com>
2023-08-10 12:19:28 +0100

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,59 +1,39 @@
 #!/usr/bin/env python3
 
-from os import getenv
+"""
+Module of Index views
+"""
+
+from flask import jsonify, abort
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import CORS
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
-
-app = Flask(__name__)
-app.register_blueprint(app_views)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
-auth_type = getenv('AUTH_TYPE', 'auth')
-if auth_type == 'auth':
-    auth = Auth()
-if auth_type == 'basic_auth':
-    auth = BasicAuth()
 
 
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """Unauthorized handler."""
-    return jsonify({"error": "Unauthorized"}), 401
-
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """Forbidden handler."""
-    return jsonify({"error": "Forbidden"}), 403
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /api/v1/status
+    Return:
+       - the status of the API
+    """
+    return jsonify({"status": "OK"})
 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """Not found handler"""
-    return jsonify({"error": "Not found"}), 404
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+       - abort(401)
+    """
+    return abort(401)
+
+# ... Keep your desired changes ...
 
 
-@app.before_request
-def authenticate_user():
-    """Authenticates a user before processing a request."""
-    if auth:
-        excluded_paths = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/',
-        ]
-        if auth.require_auth(request.path, excluded_paths):
-            auth_header = auth.authorization_header(request)
-            user = auth.current_user(request)
-            if auth_header is None:
-                abort(401)
-            if user is None:
-                abort(403)
-
-
-if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """ GET /api/v1/stats
+    Return:
+       - the number of each objects
+    """
+    stats = {}
+    # Calculate and populate stats here, if needed
+    return jsonify(stats)
