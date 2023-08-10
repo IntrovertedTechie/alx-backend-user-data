d8075d4a224f168dd5f38ee8c6282efbf4f4a602
Adio Adebayo <zillionaire234@gmail.com>
2023-08-10 14:12:22 +0100

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -19,11 +20,13 @@ if auth_type == 'auth':
 if auth_type == 'basic_auth':
     auth = BasicAuth()
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """Unauthorized handler.
     """
     return jsonify({"error": "Unauthorized"}), 401
+
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
@@ -31,11 +34,13 @@ def forbidden(error) -> str:
     """
     return jsonify({"error": "Forbidden"}), 403
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
 
 @app.before_request
 def authenticate_user():
@@ -55,6 +60,7 @@ def authenticate_user():
             if user is None:
                 abort(403)
 
+
 @app.route('/users/me', methods=['GET'], strict_slashes=False)
 def get_authenticated_user():
     """ GET /users/me
@@ -66,6 +72,7 @@ def get_authenticated_user():
     if user is None:
         abort(404)
     return jsonify(user.to_json())
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
