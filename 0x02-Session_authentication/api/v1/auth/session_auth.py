d03407300e146c5d5bbd9f01c5d583ac5431ad15
Adio Adebayo <zillionaire234@gmail.com>
2023-08-10 18:57:04 +0100

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -27,9 +27,9 @@ class SessionAuth(Auth):
         """
         if user_id is None or not isinstance(user_id, str):
             return None
-        session_id = str(uuid4())
-        self.user_id_by_session_id[session_id] = user_id
-        return session_id
+        id = uuid4()
+        self.user_id_by_session_id[str(id)] = user_id
+        return str(id)
 
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
@@ -43,6 +43,7 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
 
     def current_user(self, request=None):
         """
