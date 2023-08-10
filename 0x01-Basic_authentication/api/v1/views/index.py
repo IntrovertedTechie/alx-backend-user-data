772b49ccecb9caeba51b9ea6b4e7c55173270887
Adio Adebayo <zillionaire234@gmail.com>
2023-08-10 11:48:53 +0100

    Update index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -7,10 +7,6 @@ Module of Index views
 from flask import jsonify, abort
 from api.v1.views import app_views
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 60f5dfb25ac7d7825eb4d9abdf1ba3f728e59c41
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
@@ -19,10 +15,6 @@ def status() -> str:
     """
     return jsonify({"status": "OK"})
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 60f5dfb25ac7d7825eb4d9abdf1ba3f728e59c41
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
@@ -31,22 +23,8 @@ def unauthorized() -> str:
     """
     return abort(401)
 
-<<<<<<< HEAD
+# ... Keep your desired changes ...
 
-=======
->>>>>>> 60f5dfb25ac7d7825eb4d9abdf1ba3f728e59c41
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def forbidden() -> str:
-    """ GET /api/v1/forbidden
-    Return:
-       - abort(403)
-    """
-    return abort(403)
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 60f5dfb25ac7d7825eb4d9abdf1ba3f728e59c41
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
@@ -55,8 +33,4 @@ def stats() -> str:
     """
     stats = {}
     # Calculate and populate stats here, if needed
-<<<<<<< HEAD
     return jsonify(stats)
-=======
-    return jsonify(stats)
->>>>>>> 60f5dfb25ac7d7825eb4d9abdf1ba3f728e59c41
