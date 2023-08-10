75afe89034902128b8965b22290734ee4011dfcc
Adio Adebayo <zillionaire234@gmail.com>
2023-08-10 06:43:00 +0100

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -11,6 +11,14 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+
+# Check if AUTH_TYPE environment variable is set to 'basic_auth'
+if getenv("AUTH_TYPE") == "basic_auth":
+    auth = BasicAuth()
+else:
+    auth = Auth()
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
@@ -18,13 +26,11 @@ def unauthorized(error) -> str:
     """
     return jsonify({"error": "Unauthorized"}), 401
 
-
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
-
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -32,17 +38,7 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
-
 if __name__ == "__main__":
-    from api.v1.auth.auth import Auth
-    from api.v1.auth.basic_auth import BasicAuth
-
-    # Check if AUTH_TYPE environment variable is set to 'basic_auth'
-    if getenv("AUTH_TYPE") == "basic_auth":
-        auth = BasicAuth()
-    else:
-        auth = Auth()
-
     host = getenv("API_HOST", "0.0.0.0")
     port = int(getenv("API_PORT", "5000"))
     app.run(host=host, port=port)
