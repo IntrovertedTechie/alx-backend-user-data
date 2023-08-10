d48ebda2e647404bbb5bb7e48d6b102b88545cc8
Introvertedtechie <zillionaire234@gmail.com>
2023-08-10 15:52:29 +0000

    mercy

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Definition of class SessionAuth
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Implement Session Authorization protocol methods
+    """
+    pass
